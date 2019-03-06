30f73250f8c008d6d395c5682236b59ec8cf9ef7
Maozhu Liu <uczlm97@ucl.ac.uk>
2019-03-06 04:59:56 -0500

    adding a popup alert

--- uczlm97/www/js/userTracking.js
+++ uczlm97/www/js/userTracking.js
@@ -34,7 +34,7 @@ var lat = 51.524616;
 var lng =  -0.13818;
 // return the distance in kilometers
 var distance = calculateDistance(position.coords.latitude, position.coords.longitude, lat,lng, 'K');
-document.getElementById('showDistance').innerHTML = "Distance: " + distance;
+if (distance<0.1) {alert("You're within 100 meter of UCL.");}
 }
 
 // code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-inyour-web-apps.html
